39139d44e2e8c925819d5dbed5b6bd1209a6078c
Sheriffjoe <105744321+Sheriffjoe@users.noreply.github.com>
2022-06-22 11:48:41 +0200

    Broadcast for Server.py

--- Neu/Server.py
+++ Neu/Server.py
@@ -1,5 +1,73 @@
 import socket
 import Shared
 
-Shared.broadcast_listener()
-Shared.broadcast_sender()
+BROADCASTCODE_SERVER='820734130907390'
+BROADCASTCODE_SERVER_REPLY='482168278318180'
+
+
+SERVER=Shared.unicast_TCP_listener()
+SERVER_ADDRESS=SERVER.getsockname()
+
+SERVER_LIST=[SERVER_ADDRESS]
+CLIENT_LIST=[]
+
+ISLEADER=False
+
+
+
+#Sending Broadcasts to find other Servers/Clients
+def broadcast_sender():
+    reply=False     #If the Server doesnt receive an answer it automatically declares itself as the leader
+
+    broadcast_sender=Shared.broadcast_UDP_sender(timeout=2)     #Creating Broadcast Socket with timeout - So there is 2 Seconds between each Broadcast sent
+    print (f'Sending Broadcast on {broadcast_sender.getsockname()}')
+
+    br_addr=('<broadcast>', Shared.BROADCAST_PORT)
+
+    for i in range (0,3):
+        broadcast_sender.sendto(f'{BROADCASTCODE_SERVER},{SERVER_ADDRESS}'.encode(),br_addr)
+        print(f'[SERVER] Sending broadcast message on [{br_addr[0]}, {br_addr[1]}]')
+
+
+        #Waiting for answer of a Server
+        try:
+            data, addr= broadcast_sender.recvfrom(1024)
+            if data.startswith(BROADCASTCODE_SERVER_REPLY.encode()):
+                print("Found Server on ", data.decode().split(','))
+                reply=True      #Server has received an answer of an already active Server, which means he has to join the existing server group
+
+                #response_port = int (data.decode().split('_')[2])
+                #Shared.SERVER_LIST.append(SERVER_ADDRESS)
+                #print(f"In der Liste ist/sind {len(Shared.SERVER_LIST)} Server")
+                #leader_address=((addr[0], response_port))
+                #setup_leader(leader_address)
+                break
+        except TimeoutError:
+            pass
+
+    if reply==False:
+        global ISLEADER
+        ISLEADER=True
+        print ('[SERVER] I am the leader')
+
+
+    broadcast_sender.close()
+
+
+def broadcast_listener():
+    broadcast_listener=Shared.broadcast_UDP_listener()
+    print(f'Listening for Broadcast on {broadcast_listener.getsockname()}')
+
+    while True:
+        try:
+            data, addr = broadcast_listener.recvfrom(1024)
+        except Exception as e:
+            print(e)
+            pass
+        else:
+            if data.startswith(BROADCASTCODE_SERVER.encode()):
+                broadcast_listener.sendto(f'{BROADCASTCODE_SERVER_REPLY},{SERVER_ADDRESS}'.encode(), (addr))
+
+if __name__ == '__main__':
+    broadcast_sender()
+    broadcast_listener()
--- Neu/Shared.py
+++ Neu/Shared.py
@@ -1,5 +1,6 @@
 import socket
 
+
 BROADCAST_PORT=10001
 
 
@@ -16,26 +17,28 @@ def unicast_TCP_listener():
     s.listen()
     return s
 
-def unicast_TCP_sender():
+def unicast_TCP_sender(msg, addr):
     transmit_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transmit_socket.settimeout(1)
-    transmit_socket.connect(address)
-    transmit_socket.send(message)
+    transmit_socket.connect(addr)
+    transmit_socket.send(msg)
     transmit_socket.close()
 
 def broadcast_UDP_listener():
     s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     addr=(get_ip(),10001)
     s.bind(addr)
-    print(f"Listening on{s.getsockname()}")
     return s
 
-def broadcast_UDP_sender():
+def broadcast_UDP_sender(timeout=None):
     s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     addr=(get_ip(), 0)
     s.bind(addr)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    print(f"Broadcast Sender on {s.getsockname()}")
+    if timeout:
+        s.settimeout(timeout)
     return s
 
 def multicast_UDP_listener():
@@ -49,3 +52,6 @@ def multicast_UDP_sender():
     s.settimeout(0.2)
     s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
     return s
+
+def create_node (request_type, requester_type, addr):
+    return {'Request_Type': request_type, 'requester_type': requester_type, 'Address': addr}
